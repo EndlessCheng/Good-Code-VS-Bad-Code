15ce946363280ad219386711b24e65705f1ab52e
Σndless <loli.con@qq.com>
2014-06-18 21:24:44 +0800

    Create 逗号不是你想用，想用就能用.cpp

---
+++ 逗号不是你想用，想用就能用.cpp
@@ -0,0 +1,8 @@
+ui calcmask()
+{
+	ui mask = 0U;
+	int i;
+	For(i, 4) mask |= (And[i] ^ Or[i]) << ((3 - i) << 3);
+	for (ui Tmp = mask, mask = -1; Tmp; Tmp >>= 1) mask &= ~Tmp; /// 这相当于内部定义了mask...
+	return mask;
+}
