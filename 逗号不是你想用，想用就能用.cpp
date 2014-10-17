d9095d701cf92e05b63ae63e64ffafd923413672
Σndless <loli.con@qq.com>
2014-10-17 20:21:17 +0800

    Update 逗号不是你想用，想用就能用.cpp

--- 逗号不是你想用，想用就能用.cpp
+++ 逗号不是你想用，想用就能用.cpp
@@ -2,8 +2,8 @@ ui calcmask()
 {
 	ui mask = 0U;
 	int i;
-	For(i, 4) mask |= (And[i] ^ Or[i]) << ((3 - i) << 3);
-	for (ui Tmp = mask, mask = -1; Tmp; Tmp >>= 1) mask &= ~Tmp; /// 这相当于内部定义了mask...
+	For(i, 4) mask |= (m_and[i] ^ m_or[i]) << ((3 - i) << 3);
+	for (ui tmp = mask, mask = -1; tmp; tmp >>= 1) mask &= ~tmp; /// 这相当于内部定义了mask...
 	return mask;
 }
 
